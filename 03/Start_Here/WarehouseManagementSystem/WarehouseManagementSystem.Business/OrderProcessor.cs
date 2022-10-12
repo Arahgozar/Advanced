a8fb5fb351542967ade3f8ebeb1f223273ec0287
Arash Rahgozar <rahgozarash@gmail.com>
2022-10-11 22:22:01 -0400

    initial commit

---
+++ 03/Start_Here/WarehouseManagementSystem/WarehouseManagementSystem.Business/OrderProcessor.cs
@@ -0,0 +1,29 @@
+﻿using WarehouseManagementSystem.Domain;
+
+namespace WarehouseManagementSystem.Business
+{
+  public class OrderProcessor
+  {
+
+    public delegate void OrderInitialized();
+    public OrderInitialized OnOrderInitialized { get; set; }
+    public delegate void ProcessCompleted();
+
+    private void Initialize(Order order)
+    {
+      ArgumentNullException.ThrowIfNull(order);
+      OnOrderInitialized?.Invoke();
+    }
+
+    public void Process(Order order,ProcessCompleted onProcessCompleted=default)
+    {
+      // Run some code..
+
+      Initialize(order);
+      Console.WriteLine("XXXXXXX");
+
+      // How do I produce a shipping label?
+      onProcessCompleted?.Invoke();
+    }
+  }
+}
